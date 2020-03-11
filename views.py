ab6f61b59ce9e3e08127eca5b3c1add331f70434
tianzhao <1823984226@qq.com>
2020-03-11 10:35:55 +0800

    创建views.py文件

---
+++ views.py
@@ -0,0 +1,8 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.db.models import Q
+from django.http import HttpResponse, JsonResponse
+from .models import WeChatUser,Status,User,Reply
+# Create your views here.
+def home(request):
+        return render(request,"homepage.html")
